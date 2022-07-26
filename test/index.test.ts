dbb823dee870cb6c6115a562941ed1ea8737dc00
2nthony <ant.hu@outlook.com>
2022-07-27 00:20:38 +0800

    Revert "vitest"
    This reverts commit 59f212a2e568c154a3a25514947a45fec95b978b.

--- test/index.test.ts
+++
@@ -1,8 +0,0 @@
-import { test, expect } from "vitest";
-
-import { plus100 } from "../index";
-
-test("sync function from native code", () => {
-  const fixture = 42;
-  expect(plus100(fixture)).toBe(fixture + 100);
-});
